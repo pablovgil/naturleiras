27c21a5eec84a246ca27fdfcd5d291af0f80b37d
pablovgil <pablovgil@gmail.com>
2017-06-05 10:06:26 +0200

    Primera subida

---
+++ src/main/java/com/naturleitas/tda/TiendaDeAlimentaciónNaturleirasApplication.java
@@ -0,0 +1,12 @@
+package com.naturleitas.tda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaDeAlimentaciónNaturleirasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaDeAlimentaciónNaturleirasApplication.class, args);
+	}
+}
